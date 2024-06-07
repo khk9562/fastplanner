51afc596bd00c7cd040b5e3abe1c8273a3dd4206
stella <khk9562@naver.com>
2024-06-07 13:28:55 +0900

    user route 변경

--- routes/users.py
+++ routes/users.py
@@ -1,29 +1,36 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from models.users import User, UserSignIn
+from database.connection import Database
 
 user_router = APIRouter(
     tags=["User"]
 )
+
+user_database = Database(User)
+
 users = {}
 
 @user_router.post("/signup")
 async def sign_new_user(data: User) -> dict:
+    user_exist = await User.find_one(User.email == data.email)
     if data.email in users:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="이미 존재하는 사용자입니다."
         )
-    users[data.email] = data
+    # users[data.email] = data
+    await user_database.save(data)
     return {"message": "사용자 등록 성공"}
 
 @user_router.post("/signin")
 async def sign_user_in(user: UserSignIn) -> dict:
+    user_exist = await User.find_one(User.email == user.email)
     if user.email not in users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="존재하지 않는 사용자입니다."
         )
-    if users[user.email].password != user.password:
+    if user_exist.password != user.password:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="잚못된 비밀번호입니다."
