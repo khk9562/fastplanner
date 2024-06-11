16a20e0068d63d40d417704c3284fc64aa480e6f
stella <khk9562@naver.com>
2024-06-11 14:52:30 +0900

    테스트 함수 추가

--- tests/test_arithmetic_operations.py
+++ tests/test_arithmetic_operations.py
@@ -1,3 +1,4 @@
+# 테스트 대상 함수
 def add(a: int, b: int) -> int:
     return a + b
 
@@ -9,5 +10,20 @@ def multiply(a: int, b: int) -> int:
 
 def divide(a:int, b:int) -> int:
     return b // a
+
+
+# 테스트 대상 함수를 테스트할 테스트 함수
+def test_add() -> None:
+    assert add(1, 1) == 2
+
+def test_subtract() -> None:
+    assert substract(2, 5) == 3
+
+def test_multiply() -> None:
+    assert multiply(10, 10) == 100
+
+def test_divide() -> None:
+    return divide(25, 100) == 4
+
 
 
